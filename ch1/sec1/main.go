b3149e1fbc246ad4e1407f527ae677946b4d8c1c
MorrisFreeman <morrisfreeman0718@gmail.com>
2022-04-22 08:21:07 +0900

    implement server

--- ch1/sec1/main.go
+++ ch1/sec1/main.go
@@ -8,6 +8,26 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// // ノーマル
+	// dump, err := httputil.DumpRequest(r, true)
+	// if err != nil {
+	// 	http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+	// 	return
+	// }
+	// fmt.Println(string(dump))
+	// fmt.Fprintf(w, "<html><body>hello</body></html>")
+
+	// // クッキー使用
+	// w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	// if _, ok := r.Header["Cookie"]; ok {
+	// 	// クッキーがあるということは一度来たことがある人
+	// 	fmt.Fprintf(w, "<html><body>２回目以降</body></html>")
+	// } else {
+	// 	fmt.Fprintf(w, "<html><body>１回目</body></html>")
+	// }
+
+	// Cache-Control実験
+	w.Header().Add("Cache-Control", "max-age=180")
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
