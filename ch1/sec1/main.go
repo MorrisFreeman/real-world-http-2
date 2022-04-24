f777cc66f5516e6d31a6f5aeb5f27b50367f0693
MorrisFreeman <morrisfreeman0718@gmail.com>
2022-04-25 03:16:44 +0900

    serverの機能追加

--- ch1/sec1/main.go
+++ ch1/sec1/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -45,11 +46,49 @@ func chacheHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>hello</body></html>")
 }
 
+func slowHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(3 * time.Second)
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello</body></html>")
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Disposition", "attachment; filename=filename.txt")
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "hello")
+}
+
+func thanksHandler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, `<html>
+	<meta http-equiv="refresh" content="0;URL=./download">
+	<body>thanks</body>
+	</html>`)
+}
+
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/cookie", cookieHandler)
 	http.HandleFunc("/cache", chacheHandler)
+	http.HandleFunc("/slow", slowHandler)
+	http.HandleFunc("/download", downloadHandler)
+	http.HandleFunc("/thanks", thanksHandler)
 	log.Println("start http listening : 18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
